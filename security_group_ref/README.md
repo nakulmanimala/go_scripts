3dffb2086a575b47c7cc436438cddf4f558807d4
Nakul Prabhakaran <nakulmanimala@gmail.com>
2021-02-09 23:18:32 +0530

    security group reference check script

---
+++ security_group_ref/README.md
@@ -0,0 +1,2 @@
+# SecurityGroup Reference Checker
+Script to check, if a security group is whitelisted anywhere in the account.
---
+++ security_group_ref/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+const (
+	redColor    = "\033[1;33m%s\033[0m"
+	yellowColor = "\033[1;31m%s\033[0m"
+)
+
+func main() {
+	securityGpSet := make(map[string]string)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	svc := ec2.New(sess)
+	input := &ec2.DescribeSecurityGroupsInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("tag:PROJECT"),
+				Values: []*string{
+					aws.String("STAG-M4MARRY"),
+				},
+			},
+		},
+	}
+
+	result, err := svc.DescribeSecurityGroups(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, securityGp := range result.SecurityGroups {
+		input := &ec2.DescribeNetworkInterfacesInput{
+			Filters: []*ec2.Filter{
+				{
+					Name: aws.String("group-id"),
+					Values: []*string{
+						aws.String(*securityGp.GroupId),
+					},
+				},
+			},
+		}
+		result, err := svc.DescribeNetworkInterfaces(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf(yellowColor, *securityGp.GroupId+"\n")
+
+		for _, rSec := range result.NetworkInterfaces {
+			for _, gp := range rSec.Groups {
+				if _, ok := securityGpSet[*gp.GroupId]; !ok {
+					securityGpSet[*gp.GroupId] = *gp.GroupName
+				}
+
+			}
+		}
+
+		for k, v := range securityGpSet {
+			fmt.Printf(redColor, "	|__"+k+"["+v+"]"+"\n")
+		}
+
+		for dk := range securityGpSet {
+			delete(securityGpSet, dk)
+		}
+		fmt.Println()
+	}
+
+}
+
